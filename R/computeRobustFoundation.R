30b9d952b33de7f8f4776f1d383e7a8f57e099cf
Anthony Christidis <anthony.christidis@stat.ubc.ca>
2026-06-12 00:43:10 -0400

    refactor: separate X/y preprocessing and use PSD-guaranteed correlation estimation in Stage 1

--- R/computeRobustFoundation.R
+++ R/computeRobustFoundation.R
@@ -1,65 +1,80 @@
 #' @title Compute Robust Foundation (Internal)
 #'
 #' @description
-#' Implements Stage 1 of the FSCRE algorithm: data cleaning via DDC and robust correlation estimation.
+#' Implements Stage 1 of the FSCRE algorithm: separate data cleaning for X and y,
+#' and robust, PSD correlation estimation.
 #'
 #' @param x Design matrix.
 #' @param y Response vector.
-#' @param robust Logical.
+#' @param x_preprocess Method for X cleaning ("ddc", "none").
+#' @param y_preprocess Method for y cleaning ("wrap", "robust_z", "none").
+#' @param cor_estimator Method for correlations ("wrap", "pearson").
 #'
-#' @return A list containing the imputed data (x.imp, y.imp), the robust correlation structures (Rx, ry),
+#' @return A list containing the imputed data (x.imp, y.imp), the correlation structures (Rx, ry),
 #' and the DDC object for prediction.
 #'
 #' @keywords internal
 #'
-#' @importFrom stats cor
-#' @importFrom cellWise DDC
+#' @importFrom stats cor median mad sd
+#' @importFrom cellWise DDC wrap
 #'
-computeRobustFoundation <- function(x, y, robust) {
+computeRobustFoundation <- function(x, y, x_preprocess, y_preprocess, cor_estimator) {
 
     ddc_object <- NULL # Default
+    p <- ncol(x)
 
-    # Ensure x has column names for DDC matching
     if (is.null(colnames(x))) {
         colnames(x) <- paste0("V", 1:ncol(x))
     }
 
-    if (robust) {
-        # 1. Form joint data matrix [X, y]
-        xy_data <- cbind(x, y)
-        p <- ncol(x)
+    # _______________________________________
+    # 1. X Preprocessing (Independent of y)
+    # _______________________________________
 
-        # Ensure y column has a specific name we can track
-        colnames(xy_data)[p + 1] <- "response_y"
-
-        # 2. Run Fast DDC
-        # Uses fastDDC=TRUE as per Raymaekers & Rousseeuw (2021) for scalability
-        ddc_out <- cellWise::DDC(xy_data, DDCpars = list(fastDDC = TRUE, silent = TRUE))
-
-        # Save the object for use in predict()
+    # Prevents Target Leakage
+    if (x_preprocess == "ddc") {
+        # Run Fast DDC on predictors ONLY
+        ddc_out <- cellWise::DDC(x, DDCpars = list(fastDDC = TRUE, silent = TRUE))
         ddc_object <- ddc_out
-
-        # 3. Extract Imputed Data
-        xy_imp <- ddc_out$Ximp
-        x.imp <- xy_imp[, 1:p, drop = FALSE]
-        y.imp <- xy_imp[, p + 1]
-
-        # 4. Compute Sample Correlations on Cleaned Data
-        # "Detect-then-Analyze" paradigm
-        R_total <- cor(xy_imp)
-        Rx <- R_total[1:p, 1:p, drop = FALSE]
-        ry <- R_total[1:p, p + 1]
-
+        x.imp <- ddc_out$Ximp
     } else {
-        # Non-Robust Case
         x.imp <- x
-        y.imp <- y
+    }
 
-        # Compute standard correlations
-        Rx <- cor(x)
-        ry <- cor(x, y)
-        # Ensure ry is a vector
-        ry <- as.numeric(ry)
+    # ________________________________
+    # 2. y Preprocessing (Univariate)
+    # ________________________________
+
+    if (y_preprocess == "wrap") {
+        # cellWise::wrap expects a matrix
+        yw_out <- cellWise::wrap(as.matrix(y))
+        y.imp <- as.numeric(yw_out$Xw)
+    } else if (y_preprocess == "robust_z") {
+        med_y <- median(y)
+        mad_y <- mad(y)
+        if (mad_y == 0) mad_y <- sd(y)
+        # Simple winsorization at 3 MADs
+        y.imp <- pmin(pmax(y, med_y - 3 * mad_y), med_y + 3 * mad_y)
+    } else {
+        y.imp <- y
+    }
+
+    # ___________________________________________
+    # 3. Correlation Estimation (PSD guaranteed)
+    # ___________________________________________
+
+    if (cor_estimator == "wrap") {
+        # Raymaekers & Rousseeuw (2021): wrap then Pearson cor -> Guaranteed PSD
+        # Note: We wrap the already cleaned data to ensure maximum robustness
+        Xw <- cellWise::wrap(x.imp)$Xw
+        yw <- cellWise::wrap(as.matrix(y.imp))$Xw
+        
+        Rx <- cor(Xw)
+        ry <- as.numeric(cor(Xw, yw))
+    } else {
+        # Standard Pearson on imputed data
+        Rx <- cor(x.imp)
+        ry <- as.numeric(cor(x.imp, y.imp))
     }
 
     return(list(x.imp = x.imp,
